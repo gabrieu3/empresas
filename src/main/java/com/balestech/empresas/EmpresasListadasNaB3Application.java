a03fcc771f70deff4f4e5e43f6d50b93c42583e6
Gabriel Balestrin <gabriel.balestrin@zitrus.com.br>
2024-04-02 08:37:10 -0300

    init

---
+++ src/main/java/com/balestech/empresas/EmpresasListadasNaB3Application.java
@@ -0,0 +1,13 @@
+package com.balestech.empresas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresasListadasNaB3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresasListadasNaB3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/balestech/empresas/EmpresasListadasNaB3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balestech.empresas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresasListadasNaB3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
